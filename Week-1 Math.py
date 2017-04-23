77613637157ae77eb0db9b16b29721a8e0e1ea83
QuakeTheDuck <a1134qwerty@hotmail.com>
2017-04-23 19:47:33 -0400

    Add files via upload
    Week-1 Math

---
+++ Week-1 Math.py
@@ -0,0 +1,32 @@
+## ## M/D/Y 04/23/2017
+## Disclaimer: These are all personal notes. +=1 member - Thank you sentdex
+## Python 3.6.1
+## Screw older version of python I don't want to worry about it
+'''
+This is
+a multiline
+comment
+'''
+
+# + - * / and **
+#! You can crash python if you try to enter stupid high numbers with exponets, care
+# 99**50**4 crashed my shell
+# In older version of python, I believe 2.7 and maybe more recent version
+# The below code '1+3' would print the answer out inside of the shell when you RUN the program
+# Hit F5 and go into the python shell and enter the below code manually and it will output the answer
+1+3
+
+
+# Mathmatical functions can be directly done inside of the shell
+# Try a few yourself
+
+'''
+multiplication  *
+addition  +
+subtraction  -
+division  /
+exponets  **
+'''
+
+
+4**4
