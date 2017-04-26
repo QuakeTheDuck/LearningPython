e737dae9e90f1960a99c81bf0228c0a47e341e19
QuakeTheDuck <a1134qwerty@hotmail.com>
2017-04-26 10:03:35 -0400

    Add files via upload
    Week-1 Function parameters

---
+++ Week-1 Function Parameters.py
@@ -0,0 +1,33 @@
+## M/D/Y 04/25/2017
+## Disclaimer: These are all personal notes. +=1 member - Thank you sentdex
+## Python 3.6.1
+
+# Below we define a new function naming it 'simple_addition()'
+# We give the function two 'parameters' called 'num1' and 'num2'
+# The answer is the sum of the values, ask for num1 to be printed and the answer to be printed
+
+def simple_addition(num1,num2):
+    answer = num1 + num2
+    print('num1 is',num1)
+    print(answer)
+
+# we call the function to run it as well as define the variables 'num1=3' and 'num2=5'
+simple_addition(3,5)
+#! OR 'simple_addition(num2=5,num1=3)'
+# This will ensure the numbers are what you want rather than being assigned by iteration
+
+
+'''
+def word_join(word1,word2):
+    hello = word1,word2
+    print(hello)
+
+word_join('hello','there')
+'''
+
+'''
+def example(var1, var2):
+    print(var1,var2)
+
+example('Hey','there')
+'''
