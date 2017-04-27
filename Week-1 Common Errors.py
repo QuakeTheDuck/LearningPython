0eb8df67ed4d3e620088fb1d31dad65684f5f462
QuakeTheDuck <a1134qwerty@hotmail.com>
2017-04-26 21:05:45 -0400

    Add files via upload
    Week-1 Common errors

---
+++ Week-1 Common Errors.py
@@ -0,0 +1,53 @@
+## M/D/Y 04/26/2017
+## Disclaimer: These are all personal notes. +=1 member - Thank you sentdex
+## Python 3.6.1
+
+#!https://pythonprogramming.net/common-errors-python-3-basics/
+
+'''
+Problem solving is SOO important
+Try to figure out what is wrong before seeking other help
+Learn from mistakes and code errors
+check for typos
+
+
+'''
+
+'''
+#! EOL while scanning string literal
+x = 'ergtye5ydxer5h5d45h
+# You need to close the end of the variable, withan apostrophe
+print(x)
+'''
+
+'''
+#!QUESTION
+ Given the following:
+
+
+x = 15
+
+def task1():
+    while True:
+    print('Okay')
+
+def task2():
+    print('more tasks')
+'''
+# What is wrong?
+'''
+1. EOL while scanningstring
+2. Expected an indent block
+3. Invalid syntax
+'''
+
+
+# Below will not output an error but there is a NameError of 'ssdafsadfsafsaf
+'''
+x = 55
+
+if x > 56:
+    ssdafsadfsafsaf
+else:
+    print('hi')
+'''
