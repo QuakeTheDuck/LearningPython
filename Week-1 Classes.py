c68a3ab453ae1ba866d71ac5f51628e5ac9e8663
QuakeTheDuck <a1134qwerty@hotmail.com>
2017-04-28 20:36:22 -0400

    Add files via upload
    Week-1 Classes

---
+++ Week-1 Classes.py
@@ -0,0 +1,27 @@
+## M/D/Y 04/28/2017
+## Disclaimer: pass
+## Python 3.6.1
+
+#! Classes = Way of packaging variables and functions together
+
+class calculator:
+
+    def addition(x,y):
+        added = x + y
+        print(added)
+
+    def subtraction(x,y):
+        sub = x - y
+        print(sub)
+
+    def multiplication(x,y):
+        mult = x * y
+        print(mult)
+
+    def division(x,y):
+        div = x / y
+        print(div)
+    
+calculator.multiplication(3,7)
+
+calculator.division(50,5)
